6ac0d371d84b5d3cffe989da48e25d6aa49521f3
skyzox12z <skyzox7681@gmail.com>
2025-06-11 01:50:18 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -2,13 +2,14 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
+
 @app.route('/')
-def home (): 
-    return "Discord bot ok"
+def home():
+    return "discord bot ok"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
 
-    def keep_alive():
-        t = Thread(target=run)
-        t.start()
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
