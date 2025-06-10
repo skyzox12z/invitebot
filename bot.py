504d2158f487efb14483aba7ea6e2540c524e82b
skyzox12z <skyzox7681@gmail.com>
2025-06-11 01:53:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,15 +21,17 @@ async def on_ready():
 
 @bot.command(name="botdm")
 async def send_dm(ctx):
-    if ctx.channel.name != "cmds":
-        await ctx.send("this command can only work in `#cmds`.")
-        return
+    if isinstance(ctx.channel, discord.TextChannel):  # Vérifie si la commande est envoyée dans un salon
+        if ctx.channel.name != "cmds":
+            await ctx.send("❌ This command only work in `#cmds`.")
+            return
 
     try:
         await ctx.author.send("https://captchabynk6z.onrender.com/")
         await ctx.message.add_reaction("✅")
     except discord.Forbidden:
-        await ctx.send("I couldn't send you a dm . Check that you accept dm from this server.")
+        await ctx.send("⚠️ I couldn't send you a dm. Check that you accept dm from this server.")
 
 # Lance le bot
 bot.run(TOKEN)
+
