7fd8400d8cef27d9fb0178ce966a2240f53d50d0
skyzox12z <skyzox7681@gmail.com>
2025-06-11 02:01:03 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,8 +9,10 @@ def home():
     return "discord bot ok"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get('PORT', 8080))  # Use the PORT env var or default to 8080
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
